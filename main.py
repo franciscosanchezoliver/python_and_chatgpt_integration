1517d2869b69d834f77696171eeefe3228d1c2dd
Francisco Sanchez Oliver <spuzi>
2023-03-26 06:19:52 +0200

    Unused import :(

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from src.chatgtp.chatgpt import ChatGPT
-import os
 
 if __name__ == "__main__":
-
     chatgtp = ChatGPT(api_key="")
-
     while True:
         question = input("say something:")
         response = chatgtp.ask(question=question)
